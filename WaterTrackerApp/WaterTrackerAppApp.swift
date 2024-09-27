09953247ed3130250efcb9297c930c6159da51dd
raghad <raghadmohammed553@gmail.com>
2024-09-27 21:34:20 +0300

    Initial Commit

---
+++ WaterTrackerApp/WaterTrackerAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WaterTrackerAppApp.swift
+//  WaterTrackerApp
+//
+//  Created by Raghad on 24/03/1446 AH.
+//
+
+import SwiftUI
+
+@main
+struct WaterTrackerAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
